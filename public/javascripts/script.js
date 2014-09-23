24538418fc433f89e30c57b31f73d8fedf011576
Brennen Peters <brennen.peters@civitaslearning.com>
2014-09-23 01:29:48 -0500

    Add right sidebar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,5 @@
-var bounce_in_big = new Bounce();
-bounce_in_big
+var left_bounce_in_big = new Bounce();
+left_bounce_in_big
   .translate({
     from: { x: -400, y: 0 },
     to: { x: 0, y: 0 },
@@ -8,8 +8,8 @@ bounce_in_big
     bounces: 1
   });
 
-var bounce_in_small = new Bounce();
-bounce_in_small
+var left_bounce_in_small = new Bounce();
+left_bounce_in_small
   .translate({
     from: { x: -60, y: 0 },
     to: { x: 0, y: 0 },
@@ -18,8 +18,8 @@ bounce_in_small
     bounces: 1
   });
 
-var bounce_out_big = new Bounce();
-bounce_out_big
+var left_bounce_out_big = new Bounce();
+left_bounce_out_big
   .translate({
     from: { x: 0, y: 0 },
     to: { x: -400, y: 0 },
@@ -28,8 +28,8 @@ bounce_out_big
     bounces: 1
   });
 
-var bounce_out_small = new Bounce();
-bounce_out_small
+var left_bounce_out_small = new Bounce();
+left_bounce_out_small
   .translate({
     from: { x: 0, y: 0 },
     to: { x: -60, y: 0 },
@@ -38,27 +38,83 @@ bounce_out_small
     bounces: 1
   });
 
-$( ".menu" ).click(function() {
-  $( ".sidebar" ).show();
-  bounce_in_small.applyTo($(".sidebar")).then(function () {
 
+
+
+var right_bounce_in_big = new Bounce();
+right_bounce_in_big
+  .translate({
+    from: { x: 400, y: 0 },
+    to: { x: 0, y: 0 },
+    duration: 1000,
+    easing: "bounce",
+    bounces: 1
+  });
+
+var right_bounce_in_small = new Bounce();
+right_bounce_in_small
+  .translate({
+    from: { x: 60, y: 0 },
+    to: { x: 0, y: 0 },
+    duration: 1000,
+    easing: "bounce",
+    bounces: 1
+  });
+
+var right_bounce_out_big = new Bounce();
+right_bounce_out_big
+  .translate({
+    from: { x: 0, y: 0 },
+    to: { x: 400, y: 0 },
+    duration: 1000,
+    easing: "bounce",
+    bounces: 1
+  });
+
+var right_bounce_out_small = new Bounce();
+right_bounce_out_small
+  .translate({
+    from: { x: 0, y: 0 },
+    to: { x: 60, y: 0 },
+    duration: 1000,
+    easing: "bounce",
+    bounces: 1
+  });
+
+$( ".user" ).click(function() {
+  if ($(".sidebar-left").is(":visible")) {
+    left_bounce_out_small.applyTo($(".sidebar-left")).then(function () {
+      $( ".sidebar-left" ).hide();
+    });
+  } else {
+    $( ".sidebar-left" ).show();
+    left_bounce_in_small.applyTo($(".sidebar-left"));
+  }
+});
+
+$( ".add-mission-btn" ).click(function() {
+  $( ".add-mission" ).show();
+  left_bounce_in_big.applyTo($(".add-mission"));
+  left_bounce_out_small.applyTo($(".sidebar-left")).then(function () {
+    $( ".sidebar-left" ).hide();
   });
 });
 
-$( ".add" ).click(function() {
-  $( ".sidebar-add" ).show();
-  bounce_in_big.applyTo($(".sidebar-add")).then(function () {
-  });
-  bounce_out_small.applyTo($(".sidebar")).then(function () {
-    $( ".sidebar" ).hide();
+$( ".cancel-mission-btn" ).click(function() {
+  $( ".sidebar-left" ).show();
+  left_bounce_in_small.applyTo($(".sidebar-left"));
+  left_bounce_out_big.applyTo($(".add-mission")).then(function () {
+    $( ".add-mission" ).hide();
   });
 });
 
-$( ".cancel-add" ).click(function() {
-  $( ".sidebar" ).show();
-  bounce_in_small.applyTo($(".sidebar")).then(function () {
-  });
-  bounce_out_big.applyTo($(".sidebar-add")).then(function () {
-    $( ".sidebar-add" ).hide();
-  });
+$( ".globe" ).click(function() {
+  if ($(".sidebar-right").is(":visible")) {
+    right_bounce_out_small.applyTo($(".sidebar-right")).then(function () {
+      $( ".sidebar-right" ).hide();
+    });
+  } else {
+    $( ".sidebar-right" ).show();
+    right_bounce_in_small.applyTo($(".sidebar-right"));
+  }
 });

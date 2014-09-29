ebce4b39e7e6c6d10f5d05733f0f4f6fb6b3a7fc
Brennen Peters <brennen.peters@civitaslearning.com>
2014-09-28 22:17:06 -0500

    Itegrate with Parse

--- model/index.js
+++ model/index.js
@@ -1,74 +1,35 @@
-var pg = require('pg');
-var swigql = require('swigql');
 var _      = require('lodash');
-var CONFIG = require('config');
+var Parse = require('parse').Parse;
+Parse.initialize("AJqqFg9xC3sJPNqtsVXfrciFru214GQaAOvtu2Rk", "WIX18adoCiZ8at8fGfswLndYg9pudHw8cCfvFTIY");
 
+var Objective = Parse.Object.extend("Objective");
 
-var conString = process.env.DATABASE_URL;
-if (conString === undefined) conString = ("postgres://" + CONFIG.db.user + ":" + CONFIG.db.pass + "@" + CONFIG.db.host + "/" + CONFIG.db.database);
-
-console.log(conString);
-
-function generateSwigqlPhrasebook (baseDir, queryNames) {
-  var phrasebook = { };
-  for (var i = 0; i < queryNames.length; i++) {
-    phrasebook[queryNames[i]] = swigql.compileFile(baseDir + queryNames[i] + '.swigql', 'utf-8');
-  }
-  return phrasebook;
-}
-
-function swigqlQuery (client, templ, context) {
-  var result = templ.render(context);
-  //console.log("query: " + result[0] + " bind: " + result[1], { query: result[0], bind: result[1] });
-  var args = result.concat(_.toArray(arguments).slice(3));
-  var finals = {};
-  var starttime = process.hrtime();
-  args[2] = _.wrap(args[2], function(func, error, result) {
-    if (error) {
-      finals.error = error;
-    }
-    if (result && result.rowCount) {
-      finals.row_count = result.rowCount;
-    }
-    var startCb = process.hrtime();
-    if (_.isFunction(func)) {
-      func.apply(undefined, _.toArray(arguments).slice(1));
-    }
-  });
-  var query = client.query.apply(client, args);
-  return query;
-}
-
-var querypg = function (phrase, params, callback) {
-  if (!params) {
-    params = {};
-  }
-  pg.connect(conString, function (err, client, pg_done) {
-    if (err) {
-      pg_done();
-      return callback(err);
-    }
-    swigqlQuery(client, phrasebook[phrase], params, function (err, results) {
-      if (err) {
-        pg_done();
-        return callback(err);
-      }
-      pg_done();
-      return callback(null, results.rows);
-    });
+exports.getObjective = function (callback) {
+  var query = new Parse.Query(Objective);
+  query.find()
+  .then(function(results) {
+    console.log("Successfully retrieved " + results.length + " objectives.");
+    return callback(null, results);
+  }, function(error) {
+    console.log("Error: " + error.code + " " + error.message);
   });
 };
 
-var phrasebook  = generateSwigqlPhrasebook(__dirname + '/queries/', [
-  'test'
-]);
 
-exports.test = function (callback) {
-  querypg('test', {}, function (err, rows) {
-    if (err) {
-      return callback(err);
-    } else {
-      return callback(null, rows);
+exports.addObjective = function (name, callback) {
+  console.log(name);
+  var objective = new Objective();
+  objective.set("name", name);
+  objective.save(null, {
+    success: function(objective) {
+      // Execute any logic that should take place after the object is saved.
+      console.log('New object created with objectId: ' + objective.id);
+      return callback(null, objective.id);
+    },
+    error: function(objective, error) {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and message.
+      console.log('Failed to create new object, with error code: ' + error.message);
     }
   });
 };
--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,31 @@ var model = require('../model');
 exports.index = function(req, res) {
     res.render('index', {});
 };
+
+exports.getObjective = function(req, res) {
+  Q.all([
+    Q.ninvoke(model, 'getObjective'),
+  ])
+  .spread(function(objectives) {
+    console.log(objectives);
+    res.json(objectives);
+  })
+  .fail(function (err) {
+    return next(err);
+  });
+};
+
+
+exports.addObjective = function(req, res) {
+  console.log(req.params.name);
+  Q.all([
+    Q.ninvoke(model, 'addObjective', req.params.name),
+  ])
+  .spread(function(objective) {
+    console.log(objective);
+    res.json(objective);
+  })
+  .fail(function (err) {
+    return next(err);
+  });
+};

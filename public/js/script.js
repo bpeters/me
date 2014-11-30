40488240d9dec3ffaf1a95348df49fec698828e5
Brennen Peters <brennen.peters@civitaslearning.com>
2014-11-30 17:12:28 -0600

    removed js files

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-var left_bounce_in_big = new Bounce();
-left_bounce_in_big
-  .translate({
-    from: { x: -400, y: 0 },
-    to: { x: 0, y: 0 },
-    duration: 1000,
-    easing: "bounce",
-    bounces: 1
-  });
